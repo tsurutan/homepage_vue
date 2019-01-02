42d795baec1d595254c20c547a10aa737bc4a18c
tsurutan <ishaninaritaina623@gmail.com>
2019-01-02 16:20:13 +0900

    create base homepage

---
+++ components/Navigation.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="naviation">
+    <li class="navigation__item">
+      <a href="/">
+        プロフィール
+      </a>
+    </li>
+    <li class="navigation__item">
+      <a href="/portfolio">
+        ポートフォリオ
+      </a>
+    </li>
+    <li class="navigation__item">
+      <a href="https://www.tsurutan.com/">
+        ブログ
+      </a>
+    </li>
+  </ul>
+</template>
+
+<style>
+.naviation {
+  height: 100vh;
+  border-right: 1px solid #000;
+  list-style: none;
+  padding: 0;
+}
+
+.navigation__item {
+  margin: 0;
+  padding: 1rem 3rem;
+}
+
+</style>
---
+++ pages/portfolio/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        tsurutan_homepage
+      </h1>
+      <h2 class="subtitle">
+        My homepage created by nuxt js
+      </h2>
+    </div>
+  </div>
+</template>
